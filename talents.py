4ea502e6e2dc5e29ae0e4121de9c43340cf5e144
MorviltheWizard <jacobwntr@gmail.com>
2018-08-16 10:35:00 +0100

    Done stuff

---
+++ talents.py
@@ -0,0 +1,28 @@
+class Talents:
+    
+    
+    def __init__(self,name,description,cost)
+
+
+ self.name = 'Grit'
+ self.description = 'Gain..'
+ self.cost = 5
+
+ self.name = 'Precise Aim'
+ self.description = 'Once..'
+ self.cost = 10
+
+ self.name = 'Targeted Blow'
+ self.description = 'After..'
+ self.cost = 15
+
+ self.name = 'Stalker'
+ self.description = 'Add..'
+ self.cost = 20
+
+ self.name = 'Precise Aim'
+ self.description = 'Once..'
+ self.cost = 25
+
+
+
