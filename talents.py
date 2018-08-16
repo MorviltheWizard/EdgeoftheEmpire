ea7fc71e1a5b19105fecb4ea7073ba6c2a737c38
Doug Winter <doug.winter@isotoma.com>
2018-08-16 10:52:17 +0100

    fix talents

--- talents.py
+++ talents.py
@@ -1,27 +1,30 @@
-class Talents:
-   def __init__(self,name,description,cost)
+class Talent:
+    def __init__(self,name,description,cost):
        self.name = name
        self.description = description
        self.cost = cost
-  self.name = 'Grit'
-       self.description = 'Gain..'
-       self.cost = 5
 
-       self.name = 'Precise Aim'
-       self.description = 'Once..'
-       self.cost = 10
+talents = [
+    Talent("grit", "Some words", 5)
+    Talent("precise aim", "Some words", 10), 
+]
 
-       self.name = 'Targeted Blow'
-       self.description = 'After..'
-       self.cost = 15
+# self.name = 'Grit'
+#        self.description = 'Gain..'
+#        self.cost = 5
 
-       self.name = 'Stalker'
-       self.description = 'Add..'
-       self.cost = 20
+#        self.name = 'Precise Aim'
+#        self.description = 'Once..'
+#        self.cost = 10
 
-       self.name = 'Precise Aim'
-       self.description = 'Once..'
-       self.cost = 25
+#        self.name = 'Targeted Blow'
+#        self.description = 'After..'
+#        self.cost = 15
 
+#        self.name = 'Stalker'
+#        self.description = 'Add..'
+#        self.cost = 20
 
-     
+#        self.name = 'Precise Aim'
+#        self.description = 'Once..'
+#        self.cost = 25
